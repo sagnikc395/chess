1e6533a1d3db2622f973dcf748fc4169076c1117
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-05-08 20:47:39 +0530

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className='p-4 flex flex-col items-center'>
+      <h2 className='text-2xl'>Chess - A battle of Wits</h2>
+    </main>
+  );
+}
+
+export default App;
